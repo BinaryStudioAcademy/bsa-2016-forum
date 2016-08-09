cae75bb56f04b425c2a4ebc9a02b28c2074c407d
11001 <mylkini@yandex.ru>
2016-08-10 01:00:06 +0300

    incude rbac library

--- config/app.php
+++ config/app.php
@@ -156,6 +156,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /**
+         * External providers
+         */
+        DCN\RBAC\RBACServiceProvider::class,
     ],
 
     /*
