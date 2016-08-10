4cc594a830b1e6ac413de305b44465fac894af9c
alekseiishevchuk <shevchuka@gmail.com>
2016-08-10 18:30:52 +0300

    Rename: Table tagable to taggable

--- database/migrations/2016_08_09_205845_create_taggables_table.php
+++ database/migrations/2016_08_09_205845_create_taggables_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTagablesTable extends Migration
+class CreateTaggablesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,16 +12,16 @@ class CreateTagablesTable extends Migration
      */
     public function up()
     {
-        Schema::create('tagables', function(Blueprint $table) {
+        Schema::create('taggables', function(Blueprint $table) {
             $table->increments('id');
 
             $table->integer('tag_id')->unsigned();
             $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
 
-            $table->integer('tagable_id')->unsigned();
-            $table->foreign('tagable_id')->references('id')->on('tagables')->onDelete('cascade');
+            $table->integer('taggable_id')->unsigned();
+            $table->foreign('taggable_id')->references('id')->on('taggables')->onDelete('cascade');
 
-            $table->string('tagable_type');
+            $table->string('taggable_type');
             $table->softDeletes();
             $table->timestamps();
         });
@@ -34,11 +34,11 @@ class CreateTagablesTable extends Migration
      */
     public function down()
     {
-        Schema::table('tagables', function(Blueprint $table) {
-            $table->dropForeign('tagables_tag_id_foreign');
-            $table->dropForeign('tagables_tagable_id_foreign');
+        Schema::table('taggables', function(Blueprint $table) {
+            $table->dropForeign('taggables_tag_id_foreign');
+            $table->dropForeign('taggables_taggable_id_foreign');
         });
 
-        Schema::drop('tagables');
+        Schema::drop('taggables');
     }
 }
