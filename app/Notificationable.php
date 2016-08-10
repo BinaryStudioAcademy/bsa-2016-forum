b80ca153d6acc72b01a5f6865e67533ef9355b32
tobeinword1987 <tobeinword1987@gmail.com>
2016-08-10 18:00:20 +0300

    add Comment models and polymorphic relations

--- app/Notificationable.php
+++ app/Notificationable.php
@@ -23,4 +23,34 @@ class Notificationable extends Model
         // return the result
         return $v->passes();
     }
+
+    public function notificationableable()
+    {
+        return $this->morphTo();
+    }
+
+    public function notification()
+    {
+        return $this->belongsTo('App\Notification');
+    }
+
+    //https://laravel.com/docs/5.1/eloquent-relationships#polymorphic-relations
+
+    //to do in comments
+/*    public function notificationableables()
+    {
+        return $this->morphMany('App\Notificationable', 'notificationableable');
+    }*/
+
+    //to do in topics
+/*    public function notificationableables()
+    {
+        return $this->morphMany('App\Notificationable', 'notificationableable');
+    }*/
+
+    //to do in votes
+/*    public function notificationableables()
+    {
+        return $this->morphMany('App\Notificationable', 'notificationableable');
+    }*/
 }
