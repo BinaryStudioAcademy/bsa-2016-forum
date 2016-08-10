40e1e66c96aa706fd149b4bd3d687cda028f7aea
Oleg <voidshah@gmail.com>
2016-08-10 18:20:23 +0300

    create Topic model

---
+++ app/Topic.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Topic extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'description', 'user_id'];
+
+    /**
+     * Get topic's user
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+}
