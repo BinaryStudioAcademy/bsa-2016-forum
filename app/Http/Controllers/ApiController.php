bbaeeee74770377356b696d5a20cc84a060e4a3b
Ivan <pozharskyi@gmail.com>
2016-08-11 18:22:34 +0300

    IP/base ApiController

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    /**
+     * @var
+     */
+    protected $statusCode;
+
+    /**
+     * @return mixed
+     */
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+
+    /**
+     * @param mixed $statusCode
+     * @return $this
+     */
+    public function setStatusCode($statusCode)
+    {
+        $this->statusCode = $statusCode;
+        return $this;
+    }
+
+    /**
+     * @param $message
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function respondWithError($message)
+    {
+        return $this->respond([
+            'error' => [
+                'message' => $message,
+                'statusCode' => $this->getStatusCode()
+            ]
+        ]);
+    }
+
+    /**
+     * @param $data
+     * @param array $headers
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function respond($data, $headers = [])
+    {
+        return response()->json($data, $this->getStatusCode(), $headers);
+
+    }
+
+}
