483c870171138756949ddeeb75deb9027e9920d8
JIamep20 <cobectb16@gmail.com>
2016-08-11 19:55:22 +0300

    Mass caching templates on app init

--- resources/assets/js/app/views/topics/topics.js
+++ resources/assets/js/app/views/topics/topics.js
@@ -1,9 +1,5 @@
 var Marionette = require('backbone.marionette');
 
-var Helper = require('../../instances/Helper.js');
-
-Helper.templateCache('topics-template', 'topics.tpl');
-
 module.exports = Marionette.ItemView.extend({
-    template: '#topics-template'
+    template: '#topics.tpl'
 });
