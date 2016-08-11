3ad31188143d445ce15205e793b20f217473182c
alekseiishevchuk <shevchuka@gmail.com>
2016-08-11 15:37:41 +0300

    ShA / created: base routes

---
+++ app/Http/routes.php
@@ -0,0 +1,13 @@
+<?php
+Route::group(['middleware'=>'api', 'prefix'=>'api/v1'], function (){
+
+    Route::get('/', function (){
+        return 'Welcome to REST API v1';
+    });
+
+    Route::resource('user','UserController');
+    Route::resource('topic','TopicController');
+    Route::resource('vote','VoteController');
+
+
+});
