4fa160ab05537b6c85ed5134ec7d456556fc737d
tobeinword1987 <tobeinword1987@gmail.com>
2016-08-11 15:03:18 +0300

    remove validation

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -9,21 +9,7 @@ class Comment extends Model
     protected $fillable = ['content_origin','rating','user_id','content_generated'];
 
     protected $dates = ['deleted_at'];
-
-    private $rules = array(
-        'content_origin' => 'require',
-        'content_generated' =>'require'
-        // .. more rules here ..
-    );
-
-    public function validate($data)
-    {
-        // make a new validator object
-        $v = Validator::make($data, $this->rules);
-        // return the result
-        return $v->passes();
-    }
-
+    
     public function user()
     {
         return $this->belongsTo(User::class);
--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -10,19 +10,7 @@ class Notification extends Model
     protected $fillable = ['user_id'];
 
     protected $dates = ['deleted_at'];
-
-    private $rules = array(
-        // .. more rules here ..
-    );
-
-    public function validate($data)
-    {
-        // make a new validator object
-        $v = Validator::make($data, $this->rules);
-        // return the result
-        return $v->passes();
-    }
-
+    
     public function user()
     {
         return $this->belongsTo(User::class);
