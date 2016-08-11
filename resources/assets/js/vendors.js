d0df15180a3d04871a139949ebd7990d2f31d278
Petro Sokolnykov <info@xyz.net.ua>
2016-08-11 13:41:09 +0300

    require bootstrap js

--- resources/assets/js/vendors.js
+++ resources/assets/js/vendors.js
@@ -1,5 +1,7 @@
 $ = require('jquery');
+jQuery = $;
 _ = require('underscore');
 Backbone = require('backbone');
 Marionette = require('backbone.marionette');
-Handlebars = require('handlebars'); 
+Handlebars = require('handlebars');
+require('../../../node_modules/bootstrap-sass/assets/javascripts/bootstrap');
