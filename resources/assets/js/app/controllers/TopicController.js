9c85f9dff1ca5bb07e057d1750e877f45bc07e6d
Petro Sokolnykov <info@xyz.net.ua>
2016-08-12 17:23:04 +0300

    typo in mock name

--- resources/assets/js/app/controllers/TopicController.js
+++ resources/assets/js/app/controllers/TopicController.js
@@ -3,7 +3,7 @@ var Topics = require('../views/topics/topicCollection');
 var Marionette = require('backbone.marionette');
 var myTopicCollection = require('../collections/topicCollection');
 var logger = require('../instances/logger');
-var topics = require('../instances/mock').topics;
+var topics = require('../instances/Mock').topics;
 
 module.exports = Marionette.Object.extend({
 
