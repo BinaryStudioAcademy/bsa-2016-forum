94b79523c69e9b1e453da4f8c21220b2802ae8e8
alekseiishevchuk <shevchuka@gmail.com>
2016-08-12 22:41:56 +0300

    ShA / Add basic tests for Models: User, Topic

---
+++ tests/Models/TopicTest.php
@@ -0,0 +1,50 @@
+<?php
+
+
+class TopicTest extends TestCase
+{
+
+    public function testSave()
+    {
+        $topic = factory(App\Models\Topic::class)->create();
+        $this->seeInDatabase('topics', [
+            'id'=>$topic->id,
+            'name'=>$topic->name,
+            'description'=>$topic->description,
+            'user_id'=>$topic->user_id
+        ]);
+    }
+
+    public function testUpdate()
+    {
+        $topic = App\Models\Topic::all()->last();
+
+        $topic->reviewed_number += 1;
+        $topic->name .= 'Updated';
+        $topic->description .= 'Updated';
+        $topic->rating += 1;
+        $topic->user_id = factory(App\Models\Topic::class)->make()->user_id;;
+
+        $topic->save();
+
+        $this->seeInDatabase('topics', [
+            'id'=>$topic->id,
+            'reviewed_number'=>$topic->reviewed_number,
+            'name'=>$topic->name,
+            'description'=>$topic->description,
+            'rating'=>$topic->rating,
+            'user_id'=>$topic->user_id
+        ]);
+    }
+
+    public function testSoftDelete()
+    {
+        $topic = App\Models\Topic::all()->first();
+        $topicId = $topic->id;
+        $topic->delete();
+        $this->seeInDatabase('topics', ['id'=>$topicId])
+            ->notSeeInDatabase('topics', ['id'=>$topicId, 'deleted_at'=>null]);
+
+    }
+    
+}
---
+++ tests/Models/UserTest.php
@@ -0,0 +1,54 @@
+<?php
+
+
+class UserTest extends TestCase
+{
+
+    public function testSave()
+    {
+        $user = factory(App\Models\User::class)->create();
+        $this->seeInDatabase('users', [
+            'id'=>$user->id,
+            'first_name'=>$user->first_name,
+            'last_name'=>$user->last_name,
+            'display_name'=>$user->display_name,
+            'email'=>$user->email,
+            'reputation'=>$user->reputation,
+            'status_id'=>$user->status_id
+        ]);
+    }
+
+    public function testUpdate()
+    {
+        $user = \App\Models\User::all()->last();
+
+        $user->first_name .= 'Updated';
+        $user->last_name .= 'Updated';
+        $user->display_name .= 'Updated';
+        $user->email .= 'up';
+        $user->reputation += 1;
+        $user->status_id = 1;
+
+        $user->save();
+
+        $this->seeInDatabase('users', [
+            'id'=>$user->id,
+            'first_name'=>$user->first_name,
+            'last_name'=>$user->last_name,
+            'display_name'=>$user->display_name,
+            'email'=>$user->email,
+            'reputation'=>$user->reputation,
+            'status_id'=>$user->status_id
+        ]);
+    }
+
+    public function testSoftDelete()
+    {
+        $user = \App\Models\User::all()->first();
+        $userId = $user->id;
+        $user->delete();
+        $this->seeInDatabase('users', ['id'=>$userId])
+             ->notSeeInDatabase('users', ['id'=>$userId, 'deleted_at'=>null]);
+
+    }
+}
