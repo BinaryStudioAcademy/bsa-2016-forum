e5c060e84dcda5515db289545b7ccd2a9c72b49f
Ivan <pozharskyi@gmail.com>
2016-08-13 12:09:54 +0300

    IP/add RBAC implementation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,8 +6,13 @@ use App\Models\User;
 
 use Illuminate\Http\Request;
 
-class UserController extends ApiController
+use DCN\RBAC\Traits\HasRoleAndPermission;
+use DCN\RBAC\Contracts\HasRoleAndPermission as HasRoleAndPermissionContract;
+
+class UserController extends ApiController implements HasRoleAndPermissionContract
 {
+    use HasRoleAndPermission;
+
     /**
      * Display a listing of the resource.
      *
