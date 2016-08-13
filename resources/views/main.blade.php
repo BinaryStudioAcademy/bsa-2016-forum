39d22d38b53c42352821b9f198b25e8d0ca42b10
Petro Sokolnykov <info@xyz.net.ua>
2016-08-13 10:14:35 +0300

    use APP_DEBUG variable instead DEV in .env and main.blade

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    @if (env('DEV'))
+    @if (env('APP_DEBUG'))
         <link type="text/css" rel="stylesheet" href="css/styles.css?v={{ time() }}">
     @else
         <link type="text/css" rel="stylesheet" href="css/styles.css?v={{ env('APP_VER') }}">
@@ -13,7 +13,7 @@
 
 <div id="main"></div>
 
-@if (env('DEV'))
+@if (env('APP_DEBUG'))
     <script src="js/bundle.js?v={{ time() }}"></script>
 @else
     <script src="js/bundle.js?v={{ env('APP_VER') }}"></script>
