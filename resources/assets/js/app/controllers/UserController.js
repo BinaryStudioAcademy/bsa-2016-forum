0fe712f7f1bb1eb93981b6c412169ac781577231
Petro Sokolnykov <info@xyz.net.ua>
2016-08-14 00:33:16 +0300

    show user collection

---
+++ resources/assets/js/app/controllers/UserController.js
@@ -0,0 +1,20 @@
+var Marionette = require('backbone.marionette');
+
+module.exports = Marionette.Object.extend({
+
+    index: function () {
+        var Users = require('../collections/userCollection');
+        var UsersView = require('../views/users/userCollection');
+        var users = new Users;
+
+        users.fetch({
+            success: function (items) {
+                console.log(items);
+                App.RootView.content.show(new UsersView({
+                   collection: items
+                }));
+            }
+        });
+
+    }
+});
