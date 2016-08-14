a5bd15b8a97748f8e4321e16cd341a0b7b16e4ef
Petro Sokolnykov <info@xyz.net.ua>
2016-08-14 13:02:16 +0300

    remove trash .gitignore files, render view async

--- resources/assets/js/app/views/users/userCollection.js
+++ resources/assets/js/app/views/users/userCollection.js
@@ -1,7 +1,6 @@
 var Marionette = require('backbone.marionette');
 
 module.exports = Marionette.CollectionView.extend({
-    template: '#users',
     childView: require('./userItem'),
     childViewContainer: 'div',
     tagName: 'div'
