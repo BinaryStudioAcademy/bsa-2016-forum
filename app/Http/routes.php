fd8731a36a38b8a059645677740fe4d36effce95
alekseiishevchuk <shevchuka@gmail.com>
2016-08-15 13:35:57 +0300

    ShA / add routs for Topic/Vote/Comment/Message Attachments

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -125,6 +125,30 @@ Route::group(['middleware' => 'api', 'prefix' => 'api/v1'], function () {
         Route::delete('{attachment}', 'AttachmentController@deleteTopicAttachment')->name('deleteTopicAttachment');
     });
 
+    /*Routes for Vote attachments*/
+    Route::group(['prefix' => 'votes/{vote}/attachments'], function () {
+        Route::get('', 'AttachmentController@getAllVoteAttachments')->name('allVoteAttachments');
+        Route::get('{attachment}', 'AttachmentController@getVoteAttachment')->name('voteAttachment');
+        Route::post('{attachment}', 'AttachmentController@storeVoteAttachment')->name('storeVoteAttachment');
+        Route::delete('{attachment}', 'AttachmentController@deleteVoteAttachment')->name('deleteVoteAttachment');
+    });
+
+    /*Routes for Comment attachments*/
+    Route::group(['prefix' => 'comments/{comment}/attachments'], function () {
+        Route::get('', 'AttachmentController@getAllCommentAttachments')->name('allCommentAttachments');
+        Route::get('{attachment}', 'AttachmentController@getCommentAttachment')->name('commentAttachment');
+        Route::post('{attachment}', 'AttachmentController@storeCommentAttachment')->name('storeCommentAttachment');
+        Route::delete('{attachment}', 'AttachmentController@deleteCommentAttachment')->name('deleteCommentAttachment');
+    });
+
+    /*Routes for Message attachments*/
+    Route::group(['prefix' => 'messages/{message}/attachments'], function () {
+        Route::get('', 'AttachmentController@getAllMessageAttachments')->name('allMessageAttachments');
+        Route::get('{attachment}', 'AttachmentController@getMessageAttachment')->name('messageAttachment');
+        Route::post('{attachment}', 'AttachmentController@storeMessageAttachment')->name('storeMessageAttachment');
+        Route::delete('{attachment}', 'AttachmentController@deleteMessageAttachment')->name('deleteMessageAttachment');
+    });
+
     Route::get('rss', 'rssController@index')->name('rss');
     Route::post('rss', 'rssController@subscribe')->name('rssSubscribe');
 });
