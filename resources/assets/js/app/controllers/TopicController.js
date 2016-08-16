e183d70e2b849766a128fb98577e6b11855a5fd1
11001 <mylkini@yandex.ru>
2016-08-16 17:25:28 +0300

    detail page for topics

--- resources/assets/js/app/controllers/TopicController.js
+++ resources/assets/js/app/controllers/TopicController.js
@@ -2,11 +2,21 @@ var Marionette = require('backbone.marionette');
 var app = require('../instances/appInstance');
 var topicLayout = require('../views/topics/topicLayout');
 var TopicCollection = require('../collections/topicCollection');
+var TopicModel = require('../models/topicModel');
+var TopicDetailView = require('../views/topics/topicDetail');
 
 module.exports = Marionette.Object.extend({
     index: function () {
         var topicCollection = new TopicCollection();
         topicCollection.fetch();
         app.render(new topicLayout({collection: topicCollection}));
+    },
+    show: function (id) {
+        var topicModel = new TopicModel({id: id});
+        topicModel.fetch({
+            wait: true,
+            async: false
+        });
+        app.render(new TopicDetailView({model: topicModel}));
     }
 });
