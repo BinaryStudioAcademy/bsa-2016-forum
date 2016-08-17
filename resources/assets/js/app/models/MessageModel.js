4839b61121aee44bc04143889e1aeccce2551a61
Stepan Skopivskiy <skop.skop@yandex.ua>
2016-08-18 02:06:38 +0300

    dynamic attribute for message model

--- resources/assets/js/app/models/MessageModel.js
+++ resources/assets/js/app/models/MessageModel.js
@@ -2,7 +2,15 @@ var baseModel = require('../instances/Model');
 
 module.exports = baseModel.extend({
     urlRoot: '/messages',
-    
-    messageDirection: (this.user_from_id == 2)?'from':'to'
-    
+
+    initialize: function() {
+        this.computeAttributes();
+        this.on('change', this.computeAttributes(), this);
+    },
+
+    computeAttributes: function() {
+        this.set({
+            messageDirection:(this.get('user_from_id') == 2)?'from':'to'
+        });
+    }
 });
