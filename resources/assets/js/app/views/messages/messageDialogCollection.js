9bcd27d1b172fb30b2e92327483ce8d808d428e7
Stepan Skopivskiy <skop.skop@yandex.ua>
2016-08-19 00:56:24 +0300

    scroll animation, templates fix

--- resources/assets/js/app/views/messages/messageDialogCollection.js
+++ resources/assets/js/app/views/messages/messageDialogCollection.js
@@ -3,11 +3,17 @@ var messageDialogItem = require('./messageDialogItem');
 
 module.exports = Marionette.CollectionView.extend({
     childView: messageDialogItem,
-    childViewOptions : function () { 
-        Test = this;
+    initialize: function(){
+        this.listenTo(this.collection, "add", this.scrollDown);
+    },
+    childViewOptions : function () {
         return { 
             currentUser: this.options.currentUser,
             withUser: this.collection.getMeta().with_user
         }; 
+    },
+
+    scrollDown: function () {
+        this._parent.$el.animate({ scrollTop: this.$el.prop("scrollHeight")}, 200);
     }
 });
