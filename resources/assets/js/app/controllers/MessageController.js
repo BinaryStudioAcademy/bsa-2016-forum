34ffe906e8defa0d6d314728a5d4fa0510f74e8b
Stepan Skopivskiy <skop.skop@yandex.ua>
2016-08-18 11:41:14 +0300

    current user instance

--- resources/assets/js/app/controllers/MessageController.js
+++ resources/assets/js/app/controllers/MessageController.js
@@ -1,4 +1,6 @@
+var _ = require('underscore');
 var Marionette = require('backbone.marionette');
+var currentUser = require('../initializers/currentUser');
 var app = require('../instances/appInstance');
 var MessageLayout = require('../views/messages/messageLayout');
 var MessageCollection = require('../collections/messageCollection');
@@ -9,7 +11,7 @@ var MessageDialogLayout = require('../views/messages/messageDialogLayout');
 module.exports = Marionette.Object.extend({
     index: function () {
         var messageCollection = new MessageCollection();
-        messageCollection.parentUrl = '/users/2';
+        messageCollection.parentUrl = _.result(currentUser, 'url');
         messageCollection.fetch();
         app.render(new MessageLayout({
             collection: messageCollection
@@ -17,7 +19,7 @@ module.exports = Marionette.Object.extend({
     },
     show: function (id) {
         var messageCollection = new MessageCollection();
-        messageCollection.parentUrl = '/users/2';
+        messageCollection.parentUrl = _.result(currentUser, 'url');
         messageCollection.fetch({
             data: { with_user: id }
         });
