4f333b9082536482809919c710faec767866a649
11001 <mylkini@yandex.ru>
2016-08-22 21:13:57 +0300

    update tag service

--- app/Services/TagService.php
+++ app/Services/TagService.php
@@ -1,29 +1,23 @@
 <?php
+
 namespace App\Services;
+
 use App\Models\Tag;
+
 class TagService
 {
+
     /**
+     * Attach array of tags to topic
+     *
      * @param $taggableModel
      * @param $tags
      */
     public function TagsHandler($taggableModel, $tags)
     {
-        $tags = json_decode($tags, true);
         foreach ($tags as $tag) {
-            if (!empty($tag['id'])) {
-                $tag = Tag::find($tag['id']);
-                if (!$taggableModel->tags()->get()->find($tag['id'])) {
-                    $taggableModel->tags()->save($tag);
-                }
-            } elseif (!empty($tag['name'])) {
-                $existedTag = Tag::where('name', $tag['name'])->get()->first();
-                if ($existedTag && !$taggableModel->tags()->get()->find($existedTag->id)) {
-                    $taggableModel->tags()->save($existedTag);
-                } else {
-                    Tag::create($tag);
-                }
-            }
+            $tagEntity =  Tag::firstOrCreate(['name' => $tag]);
+            $taggableModel->tags()->save($tagEntity);
         }
     }
 }
