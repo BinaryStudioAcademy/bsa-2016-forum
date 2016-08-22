4b315d3792483192ace63b574d08de7ba44c5826
Petro Sokolnykov <info@xyz.net.ua>
2016-08-22 14:04:18 +0300

    add BookmarkModel

---
+++ resources/assets/js/app/models/BookmarkModel.js
@@ -0,0 +1,7 @@
+var baseModel = require('../instances/Model');
+
+module.exports = baseModel.extend({
+    urlRoot: '/bookmarks',
+    validate: function () {
+    }
+});
--- resources/assets/js/app/views/topics/topicDetail.js
+++ resources/assets/js/app/views/topics/topicDetail.js
@@ -1,5 +1,29 @@
 var Marionette = require('backbone.marionette');
 
 module.exports = Marionette.ItemView.extend({
-    template: 'topicDetail'
+    template: 'topicDetail',
+    events: {
+        'click #bookmark-btn': 'addBookmark'
+    },
+
+    addBookmark: function () {
+       var Bookmark = require('../../models/BookmarkModel');
+       var bookmark = new Bookmark();
+
+        $('#bookmark-btn').attr('disabled', 'disabled');
+        $('#bookmark-btn').removeClass('text-info');
+        $('#bookmark-btn').addClass('text-muted');
+
+        bookmark.save({
+            topic_id: 5,
+            user_id: 2,
+        }, {
+            success: function (response) {
+                $('#bookmark-btn').removeAttr('disabled');
+                $('#bookmark-btn').addClass('text-info');
+                $('#bookmark-btn').removeClass('text-muted');
+                $('#bookmark-btn').append(' <i class="glyphicon glyphicon-ok"></i>');$("p").append(document.createTextNode(' <i class="glyphicon glyphicon-ok"></i>'));
+            },
+        });
+    }
 });
