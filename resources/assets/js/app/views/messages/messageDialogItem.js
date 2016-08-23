96910f74d4b4d428811e1fe1ccacd287a37be7e4
Stepan Skopivskiy <skop.skop@yandex.ua>
2016-08-24 01:30:43 +0300

    delete sent message

--- resources/assets/js/app/views/messages/messageDialogItem.js
+++ resources/assets/js/app/views/messages/messageDialogItem.js
@@ -1,10 +1,28 @@
 var Marionette = require('backbone.marionette');
+var Radio = require('backbone.radio');
 
 module.exports = Marionette.ItemView.extend({
     template: 'messageDialogItem',
+    ui: {
+        delete: '.delete',
+        edit: '.edit'
+    },
+    events: {
+        'click @ui.delete' : 'clickedDeleteMessage',
+        'click @ui.edit' : 'clickedEditMessage'
+    },
+    clickedDeleteMessage: function () {
+        Radio.channel('messagesChannel').trigger('deleteMessage', this.model);
+    },
+
+    clickedEditMessage: function () {
+        Radio.channel('messagesChannel').trigger('editMessage', this.model);
+    },
+
     serializeData: function () {
         var direction = '';
         var with_user = {};
+        var edit = '';
         if(this.model.get('user_from_id') == this.options.currentUser.get('id')) {
             with_user = this.options.currentUser.toJSON();
             direction = 'from';
@@ -12,10 +30,14 @@ module.exports = Marionette.ItemView.extend({
             with_user = this.options.withUser;
             direction = 'to';
         }
+        if(this.model.get('created_at') != this.model.get('updated_at')) {
+            edit = 'Edit at';
+        }
 
         return {
             model: this.model.toJSON(),
             messageDirection: direction,
+            edit_et: edit,
             user: with_user
         }
     }
