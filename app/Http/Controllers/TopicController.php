06124a040a733fb08ae20832388528715c958907
Oleg <voidshah@gmail.com>
2016-08-23 09:42:42 +0300

    OSh/fix index action of Topic controller

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -24,8 +24,6 @@ class TopicController extends ApiController
      */
     public function index(TopicRequest $request)
     {
-        dd(\Auth::authenticate());
-
         $this->setFiltersData($request);
 
         $topics = Topic::filterByQuery($this->searchStr)->filterByTags($this->tagIds)->get();
