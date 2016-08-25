4e7634875ce70249ade5b887b278c75842444838
Petro Sokolnykov <info@xyz.net.ua>
2016-08-25 23:03:05 +0300

    change date format in topics list

--- resources/assets/js/app/views/topics/topicItem.js
+++ resources/assets/js/app/views/topics/topicItem.js
@@ -1,7 +1,13 @@
 var Marionette = require('backbone.marionette');
+var moment = require('momentjs');
 
 module.exports = Marionette.ItemView.extend({
     template: 'topicItem',
     className: 'row post-item',
-    tagName: 'div'
+    tagName: 'div',
+    
+    initialize: function () {
+        this.model.attributes.created_at =
+            moment(this.model.attributes.created_at).format('dd.MM.YYYY');
+    }
 });
