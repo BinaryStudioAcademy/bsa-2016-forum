3ae987cffd0091970da27d27075c1e72203ce58b
alekseiishevchuk <shevchuka@gmail.com>
2016-08-31 21:39:57 +0300

    ShA/infini

--- resources/assets/js/app/views/topics/topicLayout.js
+++ resources/assets/js/app/views/topics/topicLayout.js
@@ -3,6 +3,8 @@ var Marionette = require('backbone.marionette');
 var app = require('../../instances/appInstance');
 var logger = require('../../instances/logger');
 var topicCollection = require('./topicCollection');
+var InfiniScroll = require('../../../../js/vendor/infiniScroll');
+var _ = require('underscore');
 
 module.exports = Marionette.LayoutView.extend({
     template: 'topicLayout',
@@ -10,22 +12,25 @@ module.exports = Marionette.LayoutView.extend({
         container: '#posts'
     },
 
+    // initialize: function(){
+    //     this.infiniScroll = new InfiniScroll(this.collection, {success: this.appendRender});
+    // },
+
+    // remove: function() {
+    //     this.infiniScroll.destroy();
+    //     return Backbone.View.prototype.remove.call(this);
+    // },
+
     events: {
         'scroll': 'loadMoreTopics'
     },
 
     loadMoreTopics: function(){
-        var totalHeight = this.$('> div').height(),
-            scrollTop = this.$el.scrollTop() + this.$el.height(),
-            margin = 200;
-
-        // if we are closer than 'margin' to the end of the content, load more topics
-        if (scrollTop + margin >= totalHeight) {
-            MyApp.vent.trigger("search:more");
-        }
+ 
     },
 
     onRender: function () {
+        this.infiniScroll = new InfiniScroll(this.collection, {success: this.appendRender});
         this.container.show(new topicCollection({
             collection: this.collection
         }));
