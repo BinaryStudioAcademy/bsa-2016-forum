d448d6401a888a4b1846e9fffce67201468164cd
alekseiishevchuk <shevchuka@gmail.com>
2016-08-31 22:32:28 +0300

    ShA/ backbone.paginator implementation

--- resources/assets/js/app/collections/topicCollection.js
+++ resources/assets/js/app/collections/topicCollection.js
@@ -4,14 +4,50 @@ var Backbone = require("backbone");
 var PageableCollection = require("backbone.paginator");
 
 
-module.exports = baseCollection.extend({
-    url: '/categories/:catId/topics',
+module.exports = PageableCollection.extend({
+
+    //from base collection START
+    parentUrl: null,
+
+    getEntityUrl: function () {
+        return (_.result(this, 'parentUrl') || '') + _.result(this, 'url');
+    },
+
+    _getRequestUrl: function () {
+        return App.getBaseUrl() + this.getEntityUrl();
+    },
+
+    getMeta: function () {
+        return (_.result(this, '_meta'));
+    },
+
+    sync: function (method, collection, options) {
+        if (!options.url) {
+            options.url = this._getRequestUrl(collection);
+        }
+        return Backbone.sync(method, collection, options);
+    },
+
+    parse: function (response) {
+        this._meta = response._meta;
+        return response.data;
+    },
+    //from base collection END
+
     model: topicModel,
 
     initialize: function (options) {
         if (options && options.parentUrl) {
             this.parentUrl = options.parentUrl;
         }
+    },
+
+    state: {
+        firstPage: 1
+    },
+
+    queryParams: {
+        pageSize: null
     }
 
 });
--- resources/assets/js/app/views/topics/topicLayout.js
+++ resources/assets/js/app/views/topics/topicLayout.js
@@ -3,7 +3,6 @@ var Marionette = require('backbone.marionette');
 var app = require('../../instances/appInstance');
 var logger = require('../../instances/logger');
 var topicCollection = require('./topicCollection');
-var InfiniScroll = require('../../../../js/vendor/infiniScroll');
 var _ = require('underscore');
 
 module.exports = Marionette.LayoutView.extend({
@@ -12,25 +11,8 @@ module.exports = Marionette.LayoutView.extend({
         container: '#posts'
     },
 
-    // initialize: function(){
-    //     this.infiniScroll = new InfiniScroll(this.collection, {success: this.appendRender});
-    // },
-
-    // remove: function() {
-    //     this.infiniScroll.destroy();
-    //     return Backbone.View.prototype.remove.call(this);
-    // },
-
-    events: {
-        'scroll': 'loadMoreTopics'
-    },
-
-    loadMoreTopics: function(){
- 
-    },
 
     onRender: function () {
-        this.infiniScroll = new InfiniScroll(this.collection, {success: this.appendRender});
         this.container.show(new topicCollection({
             collection: this.collection
         }));
