accf10c0b3469a9ac826f99640c49221e5c2e7b9
JIamep20 <cobectb16@gmail.com>
2016-09-03 22:51:52 +0300

    Creating comment improvement

--- resources/assets/js/app/views/votes/ShowVote.js
+++ resources/assets/js/app/views/votes/ShowVote.js
@@ -1,6 +1,5 @@
 var Marionette = require('backbone.marionette');
 var Radio = require('backbone.radio');
-var AddCommentView = require('./VoteCommentItemAdd');
 var CommentsCollectionView = require('../../views/votes/VoteCommentsCollection');
 var VoteHeader = require('../../views/votes/voteHeader');
 var VoteAnswersCollectionView = require('../../views/votes/VoteAnswersCollection');
@@ -17,21 +16,18 @@ module.exports = Marionette.LayoutView.extend({
         c_count: '#count'
     },
     initialize: function () {
-        this.listenTo(Radio.channel('votesChannel'), 'setCommentsCount', function (n) {
-            this.ui.c_count.text(n + ' Comments');
+        var self = this;
+        this.options.collection.on('update', function (collection) {
+            self.ui.c_count.text(collection.length + ' Comments');
         });
     },
     onRender: function () {
+        Radio.trigger('votesChannel', 'showAddCommentView', this);
+
         this.getRegion('comments').show(
             new CommentsCollectionView({
                 collection: this.options.collection
             }));
-
-        this.getRegion('addcomment').show(
-            new AddCommentView({
-                collection: this.options.collection
-            })
-        );
 
         this.getRegion('voteheader').show(
             new VoteHeader({model: this.options.voteModel})
--- resources/assets/js/app/views/votes/VoteCommentItemAdd.js
+++ resources/assets/js/app/views/votes/VoteCommentItemAdd.js
@@ -6,11 +6,23 @@ module.exports = Marionette.ItemView.extend({
     template: 'VoteCommentAdd',
     ui: {
         addButton: '.js-confirm',
-        text: '.js-comment-text'
+        text: '.js-comment-text',
+        errorContainer: '.errors'
+    },
+    modelEvents: {
+        'invalid': function (model, errors) {
+            this.ui.errorContainer.html(errors['content_origin']);
+        }
     },
     events: {
         'click @ui.addButton': function () {
-            Radio.trigger('votesChannel', 'createComment', this);
+            var parent = this.options.parent;
+            this.model.save({content_origin: this.ui.text.val()}, {
+                success: function (data) {
+                    parent.collection.add(data);
+                    Radio.trigger('votesChannel', 'showAddCommentView', parent);
+                }
+            });
         }
     }
 });
