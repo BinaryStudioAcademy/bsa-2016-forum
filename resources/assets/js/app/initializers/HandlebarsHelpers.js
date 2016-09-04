c7957580fbb419b00bc69756800dcf651805e3ef
JIamep20 <cobectb16@gmail.com>
2016-09-04 16:36:32 +0300

    Improved styles

--- resources/assets/js/app/initializers/HandlebarsHelpers.js
+++ resources/assets/js/app/initializers/HandlebarsHelpers.js
@@ -3,9 +3,9 @@ var currentUser = require('../initializers/currentUser');
 
 module.exports = {
     register: function () {
-        Handlebars.registerHelper('deleteButton', function (model, meta) {
+        Handlebars.registerHelper('deleteButton', function (meta) {
             if (meta.deletable === true) {
-                return new Handlebars.SafeString('<button class="btn btn-md btn-danger delete-button"><span class="glyphicon glyphicon-remove-sign"></span></button>');
+                return new Handlebars.SafeString('<button class="btn btn-sm btn-danger delete-button"><span class="glyphicon glyphicon-remove-sign"></span></button>');
             }
         });
     }
