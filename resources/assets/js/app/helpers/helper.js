c5a6ea0e8f2ba79c44477a51c8f1bf2a4a29784e
Oleg <voidshah@gmail.com>
2016-09-04 21:34:31 +0300

    OSh/'isOnlySpecialCharacters' was refactored

--- resources/assets/js/app/helpers/helper.js
+++ resources/assets/js/app/helpers/helper.js
@@ -1,20 +1,7 @@
 var Helper = {
     isOnlySpecialCharacters: function (str) {
-        var len = str.length;
-        for (var i = 0; i < len; i++) {
-            switch (str.charCodeAt(i)) {
-                case 32:
-                    continue;
-                    break;
-                case 10:
-                    continue;
-                    break;
-                case 13:
-                    continue;
-                    break;
-                default:
-                    return false;
-            }
+        if (str.trim().length != 0 ) {
+            return false;
         }
         return true;
     }
