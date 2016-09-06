b3a7ac19513b167e61516cf38054ddb5c874c1a1
Nataly Kulychyk <kulychyk.nataliia@travers-group.com>
2016-09-06 23:46:03 +0300

    fix bug with fetching users data

--- app/Repositories/UserStore.php
+++ app/Repositories/UserStore.php
@@ -30,35 +30,39 @@ class UserStore implements UserStoreInterface
             if (key_exists('success', $response) && $response['success'] == false){
                 throw new ServiceUnavailableHttpException;
             }
+
             $users = array_map(
                 function ($userFunc) use ($usersInner) {
-                    $r['first_name'] = $userFunc->name;
-                    $r['last_name'] = $userFunc->surname;
-                    $r['email'] = $userFunc->email;
-                    $r['city'] = $userFunc->city;
-                    $r['country'] = $userFunc->country;
-                    $r['birthday'] = $userFunc->birthday;
-                    $r['local_id'] = $userFunc->id;
-                    $r['global_id'] = $userFunc->serverUserId;
-                    $r['avatar'] = $userFunc->avatar;
+                    $tr['global_id'] = $userFunc->serverUserId;
                     $userInner = array_filter($usersInner,
-                        function ($item) use ($r) {
-                            if ($item['global_id'] == $r['global_id']) {
+                        function ($item) use ($tr) {
+                            if ($item['global_id'] == $tr['global_id']) {
                                 return $item;
                             }
                         });
-                    $userInner = array_shift($userInner);
-                    $r['id'] = $userInner['id'];
-                    $r['display_name'] = $userInner['display_name'];
-                    $r['reputation'] = $userInner['reputation'];
-                    $r['status_id'] = $userInner['status_id'];
-                    $r['last_visit_at'] = $userInner['last_visit_at'];
-                    $r['role_id'] = $userInner['role_id'];
-                    $role = Role::where('id', $r['role_id'])->value('name');
-                    $status = Status::where('id', $r['status_id'])->value('name');
-                    $r['role'] = $role;
-                    $r['status'] = $status;
-                    return $r;
+                    if ($userInner) {
+                        $r['first_name'] = $userFunc->name;
+                        $r['last_name'] = $userFunc->surname;
+                        $r['email'] = $userFunc->email;
+                        $r['city'] = $userFunc->city;
+                        $r['country'] = $userFunc->country;
+                        $r['birthday'] = $userFunc->birthday;
+                        $r['local_id'] = $userFunc->id;
+                        $r['global_id'] = $userFunc->serverUserId;
+                        $r['avatar'] = $userFunc->avatar;
+                        $userInner = array_shift($userInner);
+                        $r['id'] = $userInner['id'];
+                        $r['display_name'] = $userInner['display_name'];
+                        $r['reputation'] = $userInner['reputation'];
+                        $r['status_id'] = $userInner['status_id'];
+                        $r['last_visit_at'] = $userInner['last_visit_at'];
+                        $r['role_id'] = $userInner['role_id'];
+                        $role = Role::where('id', $r['role_id'])->value('name');
+                        $status = Status::where('id', $r['status_id'])->value('name');
+                        $r['role'] = $role;
+                        $r['status'] = $status;
+                        return $r;
+                    }
                 },
                 $response
             );
@@ -79,9 +83,16 @@ class UserStore implements UserStoreInterface
                 },
                 $usersInner);
         }
+
+        foreach ($users as $user){
+            if ($user != null){
+                $usersNew[] = $user;
+            }
+        }
+
         return $users;
     }
-    
+
     public function get($user)
     {
         $user = $this->all($user);
