a9f8c74c1ce9c7884bd7f70c486242625ce6089d
JIamep20 <cobectb16@gmail.com>
2016-09-08 21:39:14 +0300

    Delete useless code after pull

--- resources/assets/js/app/collections/VoteRICollection.js
+++ resources/assets/js/app/collections/VoteRICollection.js
@@ -3,11 +3,5 @@ var BaseCollection = require('../instances/Collection');
 
 module.exports = BaseCollection.extend({
     model: Model,
-    url: '/voteresult',
-    initialize: function (models, options) {
-        if (options)
-            if (options.parentUrl) {
-                this.parentUrl = options.parentUrl;
-            }
-    }
+    url: '/voteresult'
 });
--- resources/assets/js/app/models/VoteRImodel.js
+++ resources/assets/js/app/models/VoteRImodel.js
@@ -1,10 +1,5 @@
 var BaseModel = require('../instances/Model');
 
 module.exports = BaseModel.extend({
-    urlRoot: '/voteresult',
-    initialize: function (data, options) {
-        if (options && options.parentUrl) {
-            this.parentUrl = options.parentUrl;
-        }
-    }
+    urlRoot: '/voteresult'
 });
