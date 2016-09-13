88e7d963216423a5dc04f737e6e33e0fc4bfefd4
alekseiishevchuk <shevchuka@gmail.com>
2016-09-13 11:57:26 +0300

    change nav menu

--- resources/assets/js/app/views/headers/navigationLayout.js
+++ resources/assets/js/app/views/headers/navigationLayout.js
@@ -1,5 +1,6 @@
 var Marionette = require('backbone.marionette');
 var NavigationCollectionView = require('./navigationCollection');
+var currentUser = require('../../initializers/currentUser');
 
 module.exports = Marionette.LayoutView.extend({
     template: 'navigationLayout',
@@ -10,6 +11,9 @@ module.exports = Marionette.LayoutView.extend({
 
     onRender: function () {
         //console.log('nav-menu layout render');
+        if(!currentUser.isAdmin()){
+            this.collection.pop()
+        }
         this.getRegion('menuContainer').show(new NavigationCollectionView({
             collection: this.collection
         }));
