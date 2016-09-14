ca3d1778f08591b7012daf9f3d46412f379428cc
Nataly Kulychyk <kulychyk.nataliia@travers-group.com>
2016-09-15 02:18:05 +0300

    fix folding vote comments

--- resources/assets/js/app/views/votes/VoteCommentsCollection.js
+++ resources/assets/js/app/views/votes/VoteCommentsCollection.js
@@ -1,5 +1,4 @@
 var Marionette = require('backbone.marionette');
-var paginateableCollectionView = require('../../instances/paginateableCollectionView');
 var childView = require('./VoteCommentItem');
 
 module.exports =  Marionette.CollectionView.extend({
