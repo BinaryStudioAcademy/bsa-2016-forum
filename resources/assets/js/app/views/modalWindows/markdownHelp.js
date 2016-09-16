6c4fcf91c300c80ead02a67824903a4ce18275aa
alekseiishevchuk <shevchuka@gmail.com>
2016-09-16 21:12:10 +0300

    optimize code logic

--- resources/assets/js/app/views/modalWindows/markdownHelp.js
+++ resources/assets/js/app/views/modalWindows/markdownHelp.js
@@ -3,9 +3,6 @@ var Marionette = require('backbone.marionette');
 module.exports = Marionette.ItemView.extend({
     template: 'markdownHelp',
     
-    show: function () {
-        this.$('.modal').modal('show');
-    },
     onRender: function() {
         this.$('.modal').modal('show');
 }
