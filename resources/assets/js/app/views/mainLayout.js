1bb8742c416aa50ecfa5517c12fb37f1ef0e15cf
alekseiishevchuk <shevchuka@gmail.com>
2016-09-18 22:13:49 +0300

    External header Integration

--- resources/assets/js/app/views/mainLayout.js
+++ resources/assets/js/app/views/mainLayout.js
@@ -13,9 +13,27 @@ var mainLayoutView = Marionette.LayoutView.extend({
         breadCrumbs: '#breadcrumbs',
         modalWindow: '#modal-container'
     },
-
+    onRender: function () {
+        var getHeader = function() {
+            var request = new XMLHttpRequest();
+            request.open('GET', 'http://team.binary-studio.com/app/header', true);
+            request.send();
+            request.onreadystatechange = function() {
+                if (request.readyState != 4) return;
+                if (request.status != 200) {
+                    alert(request.status + ': ' + request.statusText);
+                } else {
+                    var headerHtml = request.responseText;
+                    var headerContainer = document.getElementById('header');
+                    headerContainer.innerHTML =headerHtml;
+                    headerFunction();
+                }
+            };
+        };
+        getHeader();
+    },
     showRegions: function () {
-        this.getRegion('header').show(new headerView());
+        //this.getRegion('header').show(new headerView());
         this.getRegion('navigationMenu').show(new navigationLayoutView({
             collection: this.collection
         }));
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link type="text/css" rel="stylesheet" href="http://team.binary-studio.com/app/styles/css/style.css">
     @if (env('APP_DEBUG'))
         <link type="text/css" rel="stylesheet" href="css/styles.css?v={{ time() }}">
     @else
@@ -10,7 +11,7 @@
     <title>BSA16: Forum</title>
 </head>
 <body>
-
+<script src="http://team.binary-studio.com/app/javascripts/header.js"></script>
 @if (env('APP_DEBUG'))
     <script src="js/bundle.js?v={{ time() }}"></script>
 @else
