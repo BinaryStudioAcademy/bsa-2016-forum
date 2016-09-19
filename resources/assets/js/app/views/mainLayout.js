07658a82428082a60021d422cc97b0433ae8ca8c
alekseiishevchuk <shevchuka@gmail.com>
2016-09-19 09:58:17 +0300

    header prod/dev switcher

--- resources/assets/js/app/views/mainLayout.js
+++ resources/assets/js/app/views/mainLayout.js
@@ -2,6 +2,7 @@ var Marionette = require('backbone.marionette');
 var Radio = require('backbone.radio');
 var headerView = require('../views/headers/Header');
 var navigationLayoutView = require('./headers/navigationLayout');
+var config = require('config');
 
 var mainLayoutView = Marionette.LayoutView.extend({
     el: 'body',
@@ -14,26 +15,32 @@ var mainLayoutView = Marionette.LayoutView.extend({
         modalWindow: '#modal-container'
     },
     onRender: function () {
-        var getHeader = function() {
-            var request = new XMLHttpRequest();
-            request.open('GET', 'http://team.binary-studio.com/app/header', true);
-            request.send();
-            request.onreadystatechange = function() {
-                if (request.readyState != 4) return;
-                if (request.status != 200) {
-                    alert(request.status + ': ' + request.statusText);
-                } else {
-                    var headerHtml = request.responseText;
-                    var headerContainer = document.getElementById('header');
-                    headerContainer.innerHTML =headerHtml;
-                    headerFunction();
-                }
+        if(config.debug === false){
+            var getHeader = function() {
+                var request = new XMLHttpRequest();
+                request.open('GET', 'http://team.binary-studio.com/app/header', true);
+                request.send();
+                request.onreadystatechange = function() {
+                    if (request.readyState != 4) return;
+                    if (request.status != 200) {
+                        alert(request.status + ': ' + request.statusText);
+                    } else {
+                        var headerHtml = request.responseText;
+                        var headerContainer = document.getElementById('header');
+                        headerContainer.innerHTML =headerHtml;
+                        headerFunction();
+                    }
+                };
             };
-        };
-        getHeader();
+            getHeader();
+        }
+
     },
     showRegions: function () {
-        //this.getRegion('header').show(new headerView());
+        if(config.debug === true){
+            this.getRegion('header').show(new headerView());
+        }
+
         this.getRegion('navigationMenu').show(new navigationLayoutView({
             collection: this.collection
         }));
