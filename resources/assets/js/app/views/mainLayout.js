3591c6ebe219efc1a89865f5f2979afcb11fb0d8
alekseiishevchuk <shevchuka@gmail.com>
2016-09-19 13:58:01 +0300

    move externalHeaderInit to separate function

--- resources/assets/js/app/views/mainLayout.js
+++ resources/assets/js/app/views/mainLayout.js
@@ -14,25 +14,28 @@ var mainLayoutView = Marionette.LayoutView.extend({
         breadCrumbs: '#breadcrumbs',
         modalWindow: '#modal-container'
     },
+    initExternalHeader: function () {
+        var getHeader = function() {
+            var request = new XMLHttpRequest();
+            request.open('GET', config.externalHeaderLink, true);
+            request.send();
+            request.onreadystatechange = function() {
+                if (request.readyState != 4) return;
+                if (request.status != 200) {
+                    alert(request.status + ': ' + request.statusText);
+                } else {
+                    var headerHtml = request.responseText;
+                    var headerContainer = document.getElementById('header');
+                    headerContainer.innerHTML =headerHtml;
+                    headerFunction();
+                }
+            };
+        };
+        getHeader();
+    },
     onRender: function () {
         if(config.externalHeader){
-            var getHeader = function() {
-                var request = new XMLHttpRequest();
-                request.open('GET', config.externalHeaderLink, true);
-                request.send();
-                request.onreadystatechange = function() {
-                    if (request.readyState != 4) return;
-                    if (request.status != 200) {
-                        alert(request.status + ': ' + request.statusText);
-                    } else {
-                        var headerHtml = request.responseText;
-                        var headerContainer = document.getElementById('header');
-                        headerContainer.innerHTML =headerHtml;
-                        headerFunction();
-                    }
-                };
-            };
-            getHeader();
+            this.initExternalHeader();
         }
 
     },
