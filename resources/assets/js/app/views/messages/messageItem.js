9dc27c0fd5915a0e87366c015ea8ae9c0a18e37f
Stepan Skopivskiy <skop.skop@yandex.ua>
2016-09-20 23:03:27 +0300

    messages index stylization

--- resources/assets/js/app/views/messages/messageItem.js
+++ resources/assets/js/app/views/messages/messageItem.js
@@ -4,11 +4,17 @@ var helper = require('../../helpers/helper');
 
 module.exports = Marionette.ItemView.extend({
     template: 'messageItem',
+    tagName: 'li',
+    attributes: function () {
+        return {
+            class: this.options.isUserFrom ? 'from_user left' : 'by_myself right'
+        }
+    },
     serializeData: function () {
         return {
             message: helper.formatText(this.model.get('message')),
             user: this.options.user,
-            isUserFrom: this.options.isUserFrom,
+            isUserFrom: !this.options.isUserFrom,
             updatedDate: dateHelper.relativeDate(dateHelper.dateWithTimezone(this.model.get('updated_at'))),
             updatedStaticDate: dateHelper.dateWithTimezone(this.model.get('updated_at'))
         }
