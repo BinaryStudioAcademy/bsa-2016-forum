963d8ff9a8f6a5454b4b157828558c2b139b3b4b
alekseiishevchuk <shevchuka@gmail.com>
2016-09-23 08:53:15 +0300

    add helper and show summary after formatting

--- resources/assets/js/app/helpers/helper.js
+++ resources/assets/js/app/helpers/helper.js
@@ -5,6 +5,10 @@ var Helper = {
     
     formatText: function (text) {
         return text.replace(/\n/g, '<br>');
+    },
+
+    formatText2: function (text) {
+        return text.replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + '<br>' + '$2');
     }
 };
 
